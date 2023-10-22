5a357d7ca97e285c3b3352293d22c37b3a13f379
Neha Sahu <1802.nehasahu@gmail.com>
2023-10-22 11:04:47 +0530

    updated navbar buttons

--- Figma Design3/tailwind.config.js
+++ Figma Design3/tailwind.config.js
@@ -2,16 +2,7 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {
-      colors: {
-        primary: "#2d3748",
-        secondary: "#ffed4a",
-        danger: "#e3342f",
-      },
-      fontFamily: {
-        sans: ["Poppins", "sans-serif"],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
