9039aaadb4044b5cdb0def5b217be5ccb238def4
Neha Sahu <1802.nehasahu@gmail.com>
2023-10-24 20:37:17 +0530

    Client section completed

--- Figma Design3/tailwind.config.js
+++ Figma Design3/tailwind.config.js
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['"Poppins"', "sans-serif"],
-      },
-    },
-  },
-  plugins: [],
-};
+ content: ["./src/**/*.{html,js}"],
+ theme: {
+   extend: {},
+ },
+ plugins: [],
+}
