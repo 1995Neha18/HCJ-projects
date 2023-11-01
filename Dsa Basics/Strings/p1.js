81ce6b182014f7afffaf7ce02dbe1237ab98332f
Neha Sahu <1802.nehasahu@gmail.com>
2023-11-01 21:46:55 +0530

    finding the length & character of string

--- Dsa Basics/Strings/p1.js
+++ Dsa Basics/Strings/p1.js
@@ -11,5 +11,12 @@ console.log`Hello World`;
 
 const str1 = "Neha";
 console.log`Hello World ${str1} how are ${"you"}?`;
-// Note: Here we can pass any value inside the console.log and it will give an array of strings as output when using backticks 
+// Note: Here we can pass any value inside the console.log and it will give an array of strings as output when using backticks
 // & the values inside the template literal will be written as a seperate variable.
+
+// 1. Finding the length of the string.
+const str2 = "Goodmorning";
+console.log(str2.length);
+// 2. Accessing the character of the string.
+console.log(str2[3]);
+console.log(str2.charAt(5));
