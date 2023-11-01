8705c80f495c3270021521b30a9bcdc59a6c5d82
Neha Sahu <1802.nehasahu@gmail.com>
2023-11-01 20:55:22 +0530

    writing console using backticks

---
+++ Dsa Basics/Strings/p1.js
@@ -0,0 +1,8 @@
+// { Strings: When a textured data are stored inside a variable then it is known as strings. }
+
+let str = "Hello World";
+console.log(str);
+
+// Different way of wriring console.log
+
+console.log`Hello World`;
