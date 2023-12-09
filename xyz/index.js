599ec1e04877d211e3d44d1a832589315a43ac0a
Neha Sahu <1802.nehasahu@gmail.com>
2023-12-09 18:33:29 +0530

    coercion & comparison operator concept

---
+++ xyz/index.js
@@ -0,0 +1,31 @@
+// greetings();
+// console.log(x);
+
+// function greetings() {
+//   console.log("Hello Neha!");
+// }
+
+// let x = 5;
+// console.log(greetings);
+
+// --------------------------------
+// let x;
+
+// if(x ==1 && x==2 && x==3){
+//  console.log("Hello World!");
+// }
+// Assign a value to the x, so that it can satisfy all three conditions and can print "Hello World!".
+
+let x = {
+   value: 1,
+   toString: function(){
+     return this.value++;
+   }
+};
+
+if(x == 1 && x == 2 && x == 3){
+  console.log("Hello World!");
+}
+//Note: In the above code there is a comparison between Non-primitive & Primitive datatypes, 
+// Here non-primitive datatype is converted into primitive with the help of toString function.
+// So, that the value of x will be equals to 1,2 then 3 after increments.
