125693b5d8ffd5cb370587dbcd9279eaea38bbe9
Neha Sahu <1802.nehasahu@gmail.com>
2023-12-11 20:25:42 +0530

    object problem solving

--- xyz/index.js
+++ xyz/index.js
@@ -16,16 +16,22 @@
 // }
 // Assign a value to the x, so that it can satisfy all three conditions and can print "Hello World!".
 
-let x = {
-   value: 1,
-   toString: function(){
-     return this.value++;
-   }
-};
+// let x = {
+//    value: 1,
+//    toString: function(){
+//      return this.value++;
+//    }
+// };
 
-if(x == 1 && x == 2 && x == 3){
-  console.log("Hello World!");
-}
+// if(x == 1 && x == 2 && x == 3){
+//   console.log("Hello World!");
+// }
 //Note: In the above code there is a comparison between Non-primitive & Primitive datatypes, 
 // Here non-primitive datatype is converted into primitive with the help of toString function.
 // So, that the value of x will be equals to 1,2 then 3 after increments.
+
+// ---------------------xx-----------------------
+
+
+
+
