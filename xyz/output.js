d6c82ab6b9f624f7e9c3d3a149ab13611cf4f256
Neha Sahu <1802.nehasahu@gmail.com>
2023-12-12 11:04:41 +0530

    anagram problem

--- xyz/output.js
+++ xyz/output.js
@@ -22,27 +22,62 @@
 
 // console.log(typeof NAN)
 
-// Count the number of each fruits without repeating the name ofnthe fruits which occurs again.
-const fruits = [
-  { Apple: 4, Orange: 7, Grapes: 3 },
-  { Guava: 6, Lemon: 4, Banana: 8 },
-  { Orange: 5, Pineapple: 7, Apple: 7 },
-];
+// *Problem: 2
+//Count the number of each fruits without repeating the name ofnthe fruits which occurs again.
+// const fruits = [
+//   { Apple: 4, Orange: 7, Grapes: 3 },
+//   { Guava: 6, Lemon: 4, Banana: 8 },
+//   { Orange: 5, Pineapple: 7, Apple: 7 },
+// ];
 
-const countedFruits = {};
+// const countedFruits = {};
 
-fruits.forEach((element) => {
-  // console.log(element);   output: whole object will be printed
-  for (let value in element) {
-    // console.log(key);      output: all keys existing inside the object is printed
-    if (countedFruits[value]) {
-      // above object
-      countedFruits[value] = countedFruits[value] + element[value]; // output: If in the above object, there is a key which already exist then increment the value inside the key.
-    } else {
-      countedFruits[value] = element[value]; // output: If the object is having a unique key then write it.
-      // console.log(countedFruits[value]);
-    }
-  }
-});
+// fruits.forEach((element) => {
+//   // console.log(element);   output: whole object will be printed
+//   for (let value in element) {
+//     // console.log(key);      output: all keys existing inside the object is printed
+//     if (countedFruits[value]) {
+//       // above object
+//       countedFruits[value] = countedFruits[value] + element[value]; // output: If in the above object, there is a key which already exist then only increment the counting inside the key.
+//     } else {
+//       countedFruits[value] = element[value]; // output: If the key doesn't exist inside the object, then add the key as well as its value i..e, element[value].
+//       // console.log(countedFruits[value]);
+//     }
+//   }
+// });
 
-console.log(countedFruits);
+// console.log(countedFruits);
+
+// const fruits = [
+//   { Apple: 4, Orange: 7, Grapes: 3 },
+//   { Guava: 6, Lemon: 4, Banana: 8 },
+//   { Orange: 5, Pineapple: 7, Apple: 7 },
+// ];
+
+// const countedFruits = {};
+
+// fruits.forEach((element) => {
+//   for(let key in element){
+//    countedFruits[key] = countedFruits[key] ? countedFruits[key] + element[key] : element[key];
+//   }
+// });
+
+// console.log(countedFruits);
+
+// *Problem: 3
+// create a function to check if string is Anagram or not.
+
+const firstLetter = "Silent";
+const secondLetter = "Listen";
+
+function isAnagram(S1, S2){
+  let a = S1.toLowerCase();
+  let b = S2.toLowerCase();
+
+  a = a.split("").sort().join("");
+  b = b.split("").sort().join("");
+
+  return a === b;
+}
+
+console.log(isAnagram(firstLetter, secondLetter));
