b5dc233148fd09432b515f6c133cc69633feb4ff
Neha Sahu <1802.nehasahu@gmail.com>
2023-12-29 12:34:27 +0530

    closures problem-1

--- xyz/index.js
+++ xyz/index.js
@@ -26,12 +26,21 @@
 // if(x == 1 && x == 2 && x == 3){
 //   console.log("Hello World!");
 // }
-//Note: In the above code there is a comparison between Non-primitive & Primitive datatypes, 
+//Note: In the above code there is a comparison between Non-primitive & Primitive datatypes,
 // Here non-primitive datatype is converted into primitive with the help of toString function.
 // So, that the value of x will be equals to 1,2 then 3 after increments.
 
 // ---------------------xx-----------------------
 
+// { Closures } Interview asked questions :-
 
+// *Problem:1
 
-
+let count = 0;
+(function printCount() {
+  if (count === 0) {
+    let count = 1; // shadowing means here the value of count is being shadowed inside function scope
+    console.log(count); // 1
+   }
+   console.log(count); // 0
+})();
