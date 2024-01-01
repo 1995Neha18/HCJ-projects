be023f3a27a86026c126c0bc93de8acc74e2a780
Neha Sahu <1802.nehasahu@gmail.com>
2024-01-01 11:14:09 +0530

    object problem-2

--- xyz/object.js
+++ xyz/object.js
@@ -1,19 +1,30 @@
-// *Problem-1  Create a function multiplyByTwo(obj) that multiplies all numeric property values of nums by 2.
+// *Problem-1.  Create a function multiplyByTwo(obj) that multiplies all numeric property values of nums by 2.
 
-let nums = {
-  a: 100,
-  b: 200,
-  title: "My nums",
-};
+// let nums = {
+//   a: 100,
+//   b: 200,
+//   title: "My nums",
+// };
 
-function multiplyByTwo(obj){
-  for(key in obj){
-    if(obj[key] == Number){
-     // obj[key] = obj[key]*2;
-     obj[key] *= 2;
-    }
-  }
-}
+// function multiplyByTwo(obj){
+//   for(key in obj){
+//     if(obj[key] == Number){
+//      // obj[key] = obj[key]*2;
+//      obj[key] *= 2;
+//     }
+//   }
+// }
 
-multiplyByTwo(nums)
-console.log(nums);
+// multiplyByTwo(nums)
+// console.log(nums);
+
+// *Problem-2. What's the output of the following question.
+
+const a = {};
+const b = { key: "b" }; // 123
+const c = { key: "c" }; // 456
+
+a[b] = 123; // a["[object object"]
+a[c] = 456; // a["[object object"]
+
+console.log(a[b]); // 456
